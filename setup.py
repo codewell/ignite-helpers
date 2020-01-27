5f024d689836c2b539efeb691cb9d35b18e7f370
FelixAbrahamsson <felixab@kth.se>
2020-01-27 14:29:19 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
    name='ignite_helpers',
-   version='0.0.3',
+   version='0.0.4',
    description='Utility tools for PyTorch Ignite',
    author='Felix Abrahamsson',
    author_email='FelixAbrahamsson@github.com',
